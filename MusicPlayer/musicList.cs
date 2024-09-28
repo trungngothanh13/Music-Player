e462d4952409721b51ed0b5fe17fd2452a9195c1
trungngothanh13 <trungngothanh13@gmail.com>
2024-09-29 04:42:15 +0700

    Add project files.

---
+++ MusicPlayer/musicList.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Nodes;
+
+namespace MusicPlayer;
+
+public class musicList
+{
+    private songNode? head;
+
+    public void AddSong(string songName)
+    {
+        var newNode = new songNode(songName);
+        if (head == null)
+        {
+            head = newNode;
+            head.next = head; // Points to itself, creating a circular reference
+        }
+        else
+        {
+            var current = head;
+            while (current?.next != head) // Traverse to the last node
+            {
+                current = current?.next;
+            }
+            current.next = newNode; // Link the new node
+            newNode.next = head; // Make it circular
+        }
+    }
+}
---
+++ MusicPlayer/songNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Nodes;
+
+namespace MusicPlayer;
+
+public class songNode
+{
+    public string songName { get; set; }
+    public songNode? next { get; set; }
+
+    public songNode(string _songName)
+    {
+        songName = _songName;
+        next = null;
+    }
+}
